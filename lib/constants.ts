abf4c512a4ddd5774815b53401a7ffdc267f81bb
giangnt1996lc <94510657+giangnt1996lc@users.noreply.github.com>
2023-03-03 00:40:25 +0700

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const CMS_NAME = 'The Info Times'
-export const CMS_URL = 'https://theinfotimes.com'
+export const CMS_URL = 'https://news.theinfotimes.com'
 export const REDIRECT_ENABLE = true
 export const HOME_OG_IMAGE_URL = ''
